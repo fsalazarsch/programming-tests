1f7ef2f322decc3482254ffecb644f8be6dca339
fsalazarsch <fsalazar.sch@gmail.com>
2023-12-04 19:24:41 -0300

    fizzbuzz done

--- python/fizzbuzz.py
+++ python/fizzbuzz.py
@@ -1,3 +1,15 @@
 # Aquí programa una función que resuelva el problema 1 FizzBuzz
+def fizzbuzz():
+  for i in range(1, 101):
+    #print(i)
+    if i%3 == 0:
+      if i%5 == 0:
+        print("FizzBuzz")
+      else:
+        print("Fizz")
+    elif i%5 ==0:
+      print("Buzz")
+    else:
+      pass
 
-
+fizzbuzz()
